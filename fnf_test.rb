43c9aa0e96a0d288a80f91040835d481fb17f452
Julio Capote <jcapote@gmail.com>
2011-05-28 00:31:54 -0700

    added sleep in fnf_test

--- fnf_test.rb
+++ fnf_test.rb
@@ -4,6 +4,8 @@ require 'fnf'
 start_dt = Time.now
 2000.times do |i|
   Fnf::Client.queue(:get => "http://127.0.0.1:1337/#{i}")
+  #needed or we'll overrun the buffer
+  sleep 0.01
 end
 end_dt = Time.now
 puts (end_dt.to_f - start_dt.to_f)
